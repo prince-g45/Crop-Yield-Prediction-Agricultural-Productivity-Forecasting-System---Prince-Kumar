8a7cf8c2eb21c1309ab4df6b2887b8d85dbb388c
prince-g45 <231fa04g45@gmail.com>
2026-09-07 17:50:12 +0530

    Fix production API URLs

--- frontend/src/services/predictionService.js
+++ frontend/src/services/predictionService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:8000/api/v1/prediction";
+const API = "http://98.84.152.196:8000/api/v1/prediction";
 
 // ==========================================
 // Authentication Headers
